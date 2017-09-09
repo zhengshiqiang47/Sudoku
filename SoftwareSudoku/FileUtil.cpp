800ca0b042b89787f3ae5a938cb9aa46cfa62488
CoderQiang <976928202@qq.com>
2017-09-10 00:24:39 +0800

    普通数独生成，利用深度优先搜索求解。未完成:左上角第一个数

--- SoftwareSudoku/FileUtil.cpp
+++ SoftwareSudoku/FileUtil.cpp
@@ -1,8 +1,10 @@
 #include "stdafx.h"
 #include "fstream"
+#include "iostream"
 
 
 using namespace std;
+
 
 int test2(int a,int b) {
 	return a + b;
@@ -13,4 +15,20 @@ void saveFile(char* fileName, char* content) {
 	file << content;
 	file.close();
 }
+
+
+void addFile(char* fileName, char* content) {
+	ofstream file;
+	file.open(fileName,ios::app);
+	if (!file) //检查文件是否正常打开//不是用于检查文件是否存在
+	{
+		cout << "abc.txt can't open" << endl;
+		abort(); //打开失败，结束程序
+	}else {
+		file << content;
+		file.close();
+	}
+	
+}
+
 
