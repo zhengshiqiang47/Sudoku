7b7c13c7960a5ca7ff780cb64692f2500059a884
CoderQiang <976928202@qq.com>
2017-09-09 17:30:08 +0800

    封装文件输出函数

---
+++ SoftwareSudoku/FileUtil.h
@@ -0,0 +1,12 @@
+#ifndef FILEUTIL_H
+#define FILEUTIL_H
+
+using namespace std;
+
+void test2(int a, int b);
+void saveFile(char* fileName, char* content);
+
+#endif // !TEST_H
+#pragma once
+
+
--- SoftwareSudoku/SoftwareSudoku.cpp
+++ SoftwareSudoku/SoftwareSudoku.cpp
@@ -1,23 +1,18 @@
 // SoftwareSudoku.cpp: 定义控制台应用程序的入口点。
-//
 
 #include "stdafx.h"
 #include "iostream"
-#include "fstream"
+#include "FileUtil.h"
 
 using namespace std;
 
-void saveFile(char* fileName, char* content);
-
 int main()
 {
-	saveFile("sudoku.txt", "HelloWorld");
+	saveFile("sudoku.txt", "HelloWorld!!!");
     return 0;
 }
 
-void saveFile(char* fileName,char* content) {
-	ofstream file(fileName);
-	file << content;
-	file.close();
-}
+
+
+
 
