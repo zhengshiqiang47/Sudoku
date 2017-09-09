7fec4747267737926775ab3c3167b31b30a2f883
CoderQiang <976928202@qq.com>
2017-09-09 17:30:08 +0800

    封装文件输出函数

--- SoftwareSudoku/FileUtil.cpp
+++ SoftwareSudoku/FileUtil.cpp
@@ -1,7 +1,16 @@
-
 #include "stdafx.h"
+#include "fstream"
+
+
+using namespace std;
 
 int test2(int a,int b) {
 	return a + b;
+}
+
+void saveFile(char* fileName, char* content) {
+	ofstream file(fileName);
+	file << content;
+	file.close();
 }
 
